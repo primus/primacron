4c494809d3fbe252d5f12794f4513c21c5156ba2
3rd-Eden <info@3rd-Eden.com>
2013-06-10 17:04:32 +0200

    [test] Fill in the validation tests [test] Fill in tests for redis creation

--- test/scaler.test.js
+++ test/scaler.test.js
@@ -1,13 +1,29 @@
 describe('scaler', function () {
   'use strict';
 
-  var Scaler = require('../')
+  var redis = require('redis')
+    , Scaler = require('../')
     , chai = require('chai')
     , expect = chai.expect;
 
-  it('requires and creates its own redis client if none is provided');
+  it('requires and creates its own redis client if none is provided', function () {
+    var reds = redis.createClient()
+      , scale = new Scaler(reds);
+
+    // should set the same client
+    expect(scale.redis).to.equal(reds);
+
+    scale = new Scaler();
+    expect(scale.redis).to.not.equal(reds);
+    expect(scale.redis).to.be.instanceOf(redis.RedisClient);
+  });
+
   it('applies the configuration options');
-  it('is an EventEmitter');
+  it('is an EventEmitter', function () {
+    var scale = new Scaler();
+
+    expect(scale).to.be.instanceOf(require('events').EventEmitter);
+  });
 
   describe('#intercept', function () {
     it('closes WebSockets on disallowed ports');
@@ -55,9 +71,83 @@ describe('scaler', function () {
   });
 
   describe('#validate', function () {
-    it('calls the provided validated function on the validate:: namespace');
-    it('emits error::validation when the validation fails');
-    it('calls stream:: event once the event was validated');
+    it('calls the provided validated function on the validate:: namespace', function (done) {
+      var scale = new Scaler();
+
+      scale.validate('foo', function validate(data, cb) {
+        expect(data).to.equal('meh');
+        expect(cb).to.be.a('function');
+
+        done();
+      }).once('stream::foo', function () {
+        throw new Error('Broken');
+      });
+
+      scale.emit('validate::foo', 'meh');
+    });
+
+    it('emits error::validation when an error occures', function (done) {
+      var scale = new Scaler();
+
+      scale.validate('foo', function validate(data, cb) {
+        expect(data).to.equal('meh');
+        expect(cb).to.be.a('function');
+
+        cb(undefined, false);
+      });
+
+      scale.once('error::validation', function validate(event, err) {
+        expect(event).to.equal('foo');
+        expect(err).to.be.instanceOf(Error);
+        expect(err.message).to.equal('Failed to validate the data');
+
+        done();
+      }).once('stream::foo', function () {
+        throw new Error('Broken');
+      });
+
+      scale.emit('validate::foo', 'meh');
+    });
+
+    it('emits error::validation when the validation fails', function (done) {
+      var scale = new Scaler();
+
+      scale.validate('foo', function validate(data, cb) {
+        expect(data).to.equal('meh');
+        expect(cb).to.be.a('function');
+
+        cb(new Error('Failed to validate'));
+      });
+
+      scale.once('error::validation', function validate(event, err) {
+        expect(event).to.equal('foo');
+        expect(err).to.be.instanceOf(Error);
+        expect(err.message).to.equal('Failed to validate');
+
+        done();
+      }).once('stream::foo', function () {
+        throw new Error('Broken');
+      });
+
+      scale.emit('validate::foo', 'meh');
+    });
+
+    it('calls stream:: event once the event was validated successfully', function (done) {
+      var scale = new Scaler();
+
+      scale.validate('foo', function validate(data, cb) {
+        expect(data).to.equal('meh');
+        expect(cb).to.be.a('function');
+
+        cb();
+      }).once('stream::foo', function (data) {
+        expect(data).to.equal('meh');
+        expect(arguments.length).to.equal(1);
+        done();
+      });
+
+      scale.emit('validate::foo', 'meh');
+    });
   });
 
   describe('#connection', function () {
