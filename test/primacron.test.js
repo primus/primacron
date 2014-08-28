37c1702a5a82ae9f7f2fd3e73d0dc97b16d956fb
Swaagie <info@martijnswaagman.nl>
2014-08-28 14:30:26 +0200

    [test] check for presence of proxying events

--- test/primacron.test.js
+++ test/primacron.test.js
@@ -2,6 +2,7 @@ describe('Primacron', function () {
   'use strict';
 
   var Primacron = require('../')
+    , assume = require('assume')
     , portnumbers = 1024
     , server
     , reds;
@@ -15,5 +16,45 @@ describe('Primacron', function () {
 
   afterEach(function afterEach(done) {
     server.destroy(done);
+  });
+
+  describe("#listen", function () {
+    it('should register a listening event on the consumed server that re-emits', function (done) {
+       var prima = new Primacron({
+        listen: false,
+        port: ++portnumbers
+      });
+
+      prima.once('listening', function () {
+        prima.destroy();
+        done();
+      });
+
+      prima.listen();
+      assume(prima.server._events).to.have.property('listening');
+      assume(prima.server._events.listening).to.be.an('array');
+      prima.server._events.listening.forEach(function (fn) {
+        assume(fn).to.be.a('function');
+      });
+    });
+
+    it('should register a close event on the consumed server that re-emits', function (done) {
+      var prima = new Primacron({
+        listen: false,
+        port: ++portnumbers
+      });
+
+      prima.once('close', done);
+      prima.once('listening', function () {
+        prima.destroy();
+      });
+
+      prima.listen();
+      assume(prima.server._events).to.have.property('close');
+      assume(prima.server._events.close).to.be.an('array');
+      prima.server._events.close.forEach(function (fn) {
+        assume(fn).to.be.a('function');
+      });
+    });
   });
 });
